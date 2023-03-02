9f57250ceca23a59375f979efcf04d1d06d07011
Rounak33 <rounak.rai33@gmail.com>
2023-03-02 21:49:48 +0530

    done

--- src/Pages/PlayGround.jsx
+++ src/Pages/PlayGround.jsx
@@ -43,7 +43,7 @@ function PlayGround() {
       headers: {
         'content-type': 'application/json',
         'Content-Type': 'application/json',
-        'X-RapidAPI-Key': 'b4e5c5a05fmsh9adf6ec091523f8p165338jsncc58f31c26e1',
+        'X-RapidAPI-Key': '0d88c6af71msh8c3963618564664p1d5731jsn55a9b3f18a0f',
         'X-RapidAPI-Host': 'judge0-ce.p.rapidapi.com'
       },
       data: JSON.stringify({
@@ -64,7 +64,7 @@ function PlayGround() {
       url: "https://judge0-ce.p.rapidapi.com/submissions/" + token,
       params: { base64_encoded: 'true', fields: '*' },
       headers: {
-        'X-RapidAPI-Key': '3ed7a75b44mshc9e28568fe0317bp17b5b2jsn6d89943165d8',
+        'X-RapidAPI-Key': '0d88c6af71msh8c3963618564664p1d5731jsn55a9b3f18a0f',
         'X-RapidAPI-Host': 'judge0-ce.p.rapidapi.com'
       }
     };
